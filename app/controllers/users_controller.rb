4e16c01b92ee208c6d24870401caf819f5095468
Kein <kein.1945@gmail.com>
2013-05-21 23:11:59 +0400

    Restricted users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-	
+	before_filter :allowed_access?
 
 	def index
 		@users = User.all()
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	protected
+		def allowed_access?
+			unless user_signed_in? && current_user.approved?
+				respond_to do |format|
+					format.html { render :text => "Not Allowed", :status => :forbidden }
+				end
+			end
+		end
 end
