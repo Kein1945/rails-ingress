8bd97f552f1cd1ef9f4107dece3e98e83f0fc199
Kein <kein.1945@gmail.com>
2013-05-30 17:33:53 +0400

    Player activity in highchart graphs

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -6,6 +6,20 @@ class ActionsController < ApplicationController
     	redirect_to root_path
   	end
 
-  	@actions = Action.find(:all, :conditions => ['player_id = ?', @player.id], :order => 'action_at DESC', :limit=> 50)
+  	actions = Action.find(:all, :conditions => ['player_id = ?', @player.id], :order => 'action_at')
+  		# .group_by{ |r| r.action_at.strftime('%Y %m %d') }.values.flatten
+  	hourly_actions = actions.group_by{ |r| r.action_at.strftime('%H')}
+
+  	@actions = {
+  		:deploy_ap => actions.collect{ |r| { :date => r.action_at, :activity => r.deploy_ap } } ,
+  		:destroy_ap => actions.collect{ |r| { :date => r.action_at, :activity => r.destroy_ap } },
+  		:deployes => actions.collect{ |r| { :date => r.action_at, :activity => r.deployes } },
+  		:destroyes => actions.collect{ |r| { :date => r.action_at, :activity => r.destroyes } },
+  		:usage_points => actions.collect{ |r| { :date => r.action_at, :activity => r.destroyes.to_i + r.deployes.to_i } },
+  		:action_points => actions.collect{ |r| { :date => r.action_at, :activity => r.destroy_ap.to_i + r.deploy_ap.to_i } }
+  	}
+  	@hourly_actions = hourly_actions.to_a
+  		.collect{ |r| { :hour => r[0], :activity => r[1].collect{|r| r.destroyes.to_i + r.deployes.to_i}.sum } }
+  		.sort{ |a, b| a[:hour].to_i <=> b[:hour].to_i }
   end
 end
--- script/from_actions_to_stat.sql
+++ script/from_actions_to_stat.sql
@@ -1,2 +1,10 @@
 # From actions to stat
-INSERT INTO actions (action_at, player_id, deploy, destroy) SELECT FROM_UNIXTIME(FLOOR(time/1200)*1200) `at`, player_id, SUM(IF(action='deployed an', resonator,0)) `deployed`, SUM(IF(action='destroyed an', resonator, 0)) `destroyed` FROM ingt.event_res GROUP BY `at`, player_id
+INSERT INTO actions (action_at, player_id, deploy_ap, destroy_ap, deployes, destroyes)
+SELECT
+	FROM_UNIXTIME(FLOOR(TIME/3600)*3600) `at`, player_id,
+	SUM(IF(ACTION='deployed an', resonator,0)) `deploy_ap`,
+	SUM(IF(ACTION='destroyed an', resonator, 0)) `destroy_ap`,
+	COUNT(IF(ACTION='deployed an', resonator,NULL)) `deployes`,
+	COUNT(IF(ACTION='destroyed an', resonator, NULL)) `destroyes`
+FROM `ingress_tmp`.event_res
+GROUP BY `at`, player_id
